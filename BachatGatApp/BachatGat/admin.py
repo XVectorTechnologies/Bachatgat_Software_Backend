dce7795e8f705fa371b092d08aa88675ab0a0b05
XVectorTechnologies <maheshwarisuryawanshixz1@gmail.com>
2024-10-13 17:40:55 +0530

    implement a model and serialize for user , Bank,loan and saving

--- BachatGatApp/BachatGat/admin.py
+++ BachatGatApp/BachatGat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(BachatGatRegistration)
+admin.site.register(MemberRegistration)
+admin.site.register(UserDetail)
+admin.site.register(BankDetail)
+admin.site.register(Saving)
+admin.site.register(Loan)
+admin.site.register(FundDistribution)
