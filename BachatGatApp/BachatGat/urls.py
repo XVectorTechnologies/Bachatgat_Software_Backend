9086e7ff750f06a0e1213df90577a565b766f9f4
XVectorTechnologies <maheshwarisuryawanshixz1@gmail.com>
2024-10-15 20:16:13 +0530

    conflict resolve

---
+++ BachatGatApp/BachatGat/urls.py
@@ -0,0 +1,19 @@
+from django.urls import *
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+
+router = DefaultRouter()
+router.register("bachatgat", BachatGatRegistrationViewSet),
+router.register("member_registration",MemberRegistrationViewSet),
+router.register("bank_detail",BankDetailViewSet),
+router.register("users",UserDetailViewSet),
+router.register("saving_user",SavingsViewSet),
+router.register("fund_distribution",FundDistributionViewSet),
+router.register("meeting_",MeetingViewSet),
+
+urlpatterns = [
+    *router.urls,
+    path('',include(router.urls)),
+
+]
